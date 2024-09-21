8518f88a6ed67009819a9fbd59eb6048a51e580c
goyalshreyansh21 <91679502+goyalshreyansh21@users.noreply.github.com>
2024-09-21 23:02:45 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -14,52 +14,46 @@ function App() {
 
   useEffect(() => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const loadProvider = async () => {
-      try {
-        if (provider) {
-          window.ethereum.on("chainChanged", () => {
-            window.location.reload();
-          });
-  
-          window.ethereum.on("accountsChanged", () => {
-            window.location.reload();
-          });
-          await provider.send("eth_requestAccounts", []);
-          const signer = provider.getSigner();
-          const address = await signer.getAddress();
-          setAccount(address);
-          
-          let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
-          const contract = new ethers.Contract(
-            contractAddress,
-            Upload.abi,
-            signer
-          );
-          console.log(contract);
-          setContract(contract);
-          setProvider(provider);
-        } else {
-          console.error("Metamask is not installed");
-        }
-      } catch (error) {
-        console.error("Error loading provider:", error);
+    const loadProvider = async () => {
+      if (provider) {
+        window.ethereum.on("chainChanged", () => {
+          window.location.reload();
+        });
+
+        window.ethereum.on("accountsChanged", () => {
+          window.location.reload();
+        });
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+        const contract = new ethers.Contract(
+          contractAddress,
+          Upload.abi,
+          signer
+        );
+        //console.log(contract);
+        setContract(contract);
+        setProvider(provider);
+      } else {
+        console.error("Metamask is not installed");
       }
     };
-
-    if (provider) loadProvider();
+    provider && loadProvider();
   }, []);
-
   return (
     <>
-      {/* {!modalOpen && (
+      {!modalOpen && (
         <button className="share" onClick={() => setModalOpen(true)}>
           Share
         </button>
       )}
       {modalOpen && (
         <Modal setModalOpen={setModalOpen} contract={contract}></Modal>
-      )} */}
+      )}
 
       <div className="App">
         <h1 style={{ color: "white" }}>Gdrive 3.0</h1>
@@ -75,7 +69,7 @@ function App() {
           provider={provider}
           contract={contract}
         ></FileUpload>
-     
+        <Display contract={contract} account={account}></Display>
       </div>
     </>
   );
