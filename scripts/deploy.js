95207b44abc9ef4827054537e7ac2533ad09c080
goyalshreyansh21 <91679502+goyalshreyansh21@users.noreply.github.com>
2024-09-21 20:15:58 +0530

    commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,12 +5,14 @@ async function main() {
   console.log("Deploying contract with account:", deployer.address);
 
   const balance = await deployer.provider.getBalance(deployer.address);
-  console.log("Account balance:", hre.ethers.formatEther(balance), "ETH");
+  console.log("Account balance:", hre.ethers.utils.formatEther(balance), "ETH");
 
   const Upload = await hre.ethers.getContractFactory("Upload");
   const upload = await Upload.deploy();
 
-  console.log("Upload contract deployed to:", upload.target);
+  await upload.deployed();
+
+  console.log("Upload contract deployed to:", upload.address);
 }
 
 main()
