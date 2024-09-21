76ac240f3ed8240b026e6166a660d5836bc87612
goyalshreyansh21 <91679502+goyalshreyansh21@users.noreply.github.com>
2024-09-22 00:02:21 +0530

    Update Display.js

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,44 +1,40 @@
 import { useState } from "react";
 import "./Display.css";
-const Display = ({ contract, account }) => {
-  const [data, setData] = useState("");
-  const getdata = async () => {
-    let dataArray;
-    const Otheraddress = document.querySelector(".address").value;
-    try {
-      if (Otheraddress) {
-        dataArray = await contract.display(Otheraddress);
-        console.log(dataArray);
-      } else {
-        dataArray = await contract.display(account);
-      }
-    } catch (e) {
-      alert("You don't have access");
-    }
-    const isEmpty = Object.keys(dataArray).length === 0;
 
-    if (!isEmpty) {
-      const str = dataArray.toString();
-      const str_array = str.split(",");
-      // console.log(str);
-      // console.log(str_array);
-      const images = str_array.map((item, i) => {
-        return (
-          <a href={item} key={i} target="_blank">
-            <img
-              key={i}
-              src={`https://gateway.pinata.cloud/ipfs/${item.substring(6)}`}
-              alt="new"
-              className="image-list"
-            ></img>
-          </a>
-        );
-      });
-      setData(images);
-    } else {
-      alert("No image to display");
+const Display = ({ contract, account }) => {
+  const [data, setData] = useState([]);
+  const [otherAddress, setOtherAddress] = useState("");
+
+  const getData = async () => {
+    let dataArray;
+
+    try {
+      dataArray = otherAddress ? await contract.display(otherAddress) : await contract.display(account);
+    } catch (e) {
+      alert("You don't have access or an error occurred.");
+      console.error("Error fetching data:", e);
+      return; 
     }
+
+    if (dataArray.length === 0) {
+      alert("No images to display.");
+      setData([]); 
+      return;
+    }
+
+    const images = dataArray.map((item, i) => (
+      <a href={item} key={i} target="_blank" rel="noopener noreferrer">
+        <img
+          src={`https://gateway.pinata.cloud/ipfs/${item.substring(6)}`}
+          alt={`Uploaded content ${i}`}
+          className="image-list"
+        />
+      </a>
+    ));
+
+    setData(images);
   };
+
   return (
     <>
       <div className="image-list">{data}</div>
@@ -46,11 +42,14 @@ const Display = ({ contract, account }) => {
         type="text"
         placeholder="Enter Address"
         className="address"
-      ></input>
-      <button className="center button" onClick={getdata}>
+        value={otherAddress}
+        onChange={(e) => setOtherAddress(e.target.value)}
+      />
+      <button className="center button" onClick={getData}>
         Get Data
       </button>
     </>
   );
 };
+
 export default Display;
